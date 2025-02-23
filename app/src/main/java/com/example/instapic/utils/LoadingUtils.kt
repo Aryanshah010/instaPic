6f1e46ab691e022193e669e70b47bbb05e7a1a14
Aryanshah010 <shaharyan844@gmail.com>
2025-02-23 11:07:54 +0545

    Login and signup

--- app/src/main/java/com/example/instapic/utils/LoadingUtils.kt
+++ app/src/main/java/com/example/instapic/utils/LoadingUtils.kt
@@ -4,23 +4,24 @@ import android.app.Activity
 import android.app.AlertDialog
 import com.example.instapic.R
 
+
 class LoadingUtils(val activity: Activity) {
     lateinit var alertDialog: AlertDialog
 
     fun show(){
-        val builder = AlertDialog.Builder(activity)
+        val builder=AlertDialog.Builder(activity)
 
-        val dialogView = activity.layoutInflater.inflate(
+        val dialogView=activity.layoutInflater.inflate(
             R.layout.loading,null)
-
         builder.setView(dialogView)
         builder.setCancelable(false)
 
-        alertDialog = builder.create()
+        alertDialog=builder.create()
         alertDialog.show()
     }
 
     fun dismiss(){
         alertDialog.dismiss()
+
     }
 }
